7b857ee50bb9fe197c034f725d130f8a78deaa1f
pd6974 <pdolan17@gmail.com>
2022-11-04 09:35:29 -0500

    Completed exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let x = 0;
+  for (let i = 0; i < a.length; i++) {
+    x += a[i];
+  }
+  return x;
+}
+
+const multiply = function(a) {
+  let x = 1;
+  for (let i = 0; i < a.length; i++) {
+    x *= a[i];
+  }
+  return x;
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+	return a ** b;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a <= 1) {
+    return 1;
+  } else {
+    return a * factorial(a - 1);
+  }
 };
 
 // Do not edit below this line
