b0758c0138ccec77df5e318f04f082d938f7bfee
pd6974 <pdolan17@gmail.com>
2022-11-05 17:27:45 -0500

    Completed lesson 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const oldestSort = arr.sort(function(a, b) {
+        const today = new Date();
+        const thisYear = today.getFullYear();
+        if (a.yearOfDeath === undefined) {
+            const lastPers = thisYear - a.yearOfBirth;
+            const nextPers = b.yearOfDeath - b.yearOfBirth;
+            return lastPers > nextPers ? -1 : 1;
+        } else if (b.yearOfDeath === undefined) {
+            const lastPers = a.yearOfDeath - a.yearOfBirth;
+            const nextPers = thisYear - b.yearOfBirth;
+            return lastPers > nextPers ? -1 : 1;
+        } else{
+            const lastPers = a.yearOfDeath - a.yearOfBirth;
+            const nextPers = b.yearOfDeath - b.yearOfBirth;
+            return lastPers > nextPers ? -1 : 1;
+        }
+    })
+    return oldestSort[0];
 };
 
 // Do not edit below this line
