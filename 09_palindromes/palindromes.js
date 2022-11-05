b6993388fbe29a6ef0c1f172eeb351e7326ac0c8
pd6974 <pdolan17@gmail.com>
2022-11-05 12:54:09 -0500

    Completed lesson 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replace(/\s/g, '');
+    let str2 = '';
+    for (let i = 0; i < str.length; i++) {
+        if(str[i].match(/[a-zA-Z]/i)) {
+            str2 += str[i].toLowerCase();
+        } else {
+            
+        }
+    }
 
+    for (let i = 0; i < (str2.length/2); i++) {
+        if(str2[i] !== str2[str2.length - (1 + i)]) {
+            return false;
+        }
+        
+    }
+    return true;
 };
 
 // Do not edit below this line
